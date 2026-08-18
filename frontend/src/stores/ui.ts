08b06d80b728a74c33d8a1754bc30c92a04453c6
xiemingjie <2960653141@qq.com>
2026-08-18 23:27:21 +0800

    feat: force light display mode

--- frontend/src/stores/ui.ts
+++ frontend/src/stores/ui.ts
@@ -14,7 +14,6 @@ export type RunCompletionNotice = {
 
 export const useUiStore = defineStore('ui', () => {
   const sessionDrawerOpen = ref(false)
-  const themeMenuOpen = ref(false)
   const detailOpen = ref(false)
   const detailTitle = ref('')
   const detailKind = ref<DetailKind>('citation')
@@ -39,10 +38,9 @@ export const useUiStore = defineStore('ui', () => {
   const closeTopLayer = () => {
     if (detailOpen.value) detailOpen.value = false
     else if (sessionDrawerOpen.value) sessionDrawerOpen.value = false
-    else if (themeMenuOpen.value) themeMenuOpen.value = false
   }
   return {
-    sessionDrawerOpen, themeMenuOpen, detailOpen, detailTitle, detailKind, detailPayload,
+    sessionDrawerOpen, detailOpen, detailTitle, detailKind, detailPayload,
     completionNotices, openDetail, openCitation, notifyRunCompleted, dismissRunNotice, closeTopLayer,
   }
 })
